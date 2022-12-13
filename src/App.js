53668f881f71afcdfa1fa95796c476f56de6f9f0
cheguidev <redmagicman67@outlook.com>
2022-12-13 21:17:19 +0300

    Added LoginButton, Navbar and Dependencies.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import Navbar from "./components/Navbar";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e1667",
+    },
+    secondary: {
+      main: "#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto"],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: "2rem",
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: "2rem",
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-     Ana Sayfa
+      <ThemeProvider theme={theme}>
+        <Navbar />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import LoginBtn from "./LoginBtn";
+import logo from "../logo.svg";
+import logoMobile from "../logoMobile.svg";
+import { Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const styles = makeStyles({
+  bar: {
+    paddingTop: "1.15rem",
+    backgroundColor: "#b5e7f5",
+    ["@media (max-width:780px)"]: {
+      flexDirection: "column",
+    },
+  },
+  logo: {
+    width: "6%",
+    ["@media (max-width:780px)"]: {
+      display: "none",
+    },
+  },
+  logoMobile: {
+    width: "100%",
+    display: "none",
+    ["@media (max-width:780px)"]: {
+      display: "inline-block",
+    },
+  },
+  menuItem: {
+    cursor: "pointer",
+    flexGrow: 1,
+    "&:hover": {
+      color: "#4f25c8",
+    },
+    ["@media (max-width:780px)"]: {
+      paddingBottom: "1rem",
+    },
+  },
+});
+
+function Navbar() {
+  const classes = styles();
+  return (
+    <Toolbar
+      position="sticky"
+      color="rgba(0, 0, 0, 0.87)"
+      className={classes.bar}
+    >
+      <img src={logo} className={classes.logo} />
+      <img src={logoMobile} className={classes.logoMobile} />
+      <Typography variant="h4" className={classes.menuItem}>
+        Yeşil Zincir
+      </Typography>
+      <Typography variant="h4" className={classes.menuItem}>
+        Vizyon ve Misyon
+      </Typography>
+      <Typography variant="h4" className={classes.menuItem}>
+        Hakkımızda
+      </Typography>
+      <Typography variant="h4" className={classes.menuItem}>
+        Bize Ulaşın
+      </Typography>
+      <LoginBtn txt="Giriş Yap" />
+    </Toolbar>
+  );
+}
+
+export default Navbar;
