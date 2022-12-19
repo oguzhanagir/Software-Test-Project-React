68222b11befdae42c656738a5e7f1baf64e5d25e
cheguidev <redmagicman67@outlook.com>
2022-12-19 12:31:41 +0300

    Added UserTbl

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,36 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import UserTbl from "../components/UserTbl";
+import { makeStyles } from "@material-ui/core";
 
-// Example of a data array that
-// you might receive from an API
-const data = [
-  { name: "Ahmet", surname: "Yılmaz", age: 19, gender: "Male" },
-  { name: "Ahmet", surname: "Yılmaz", age: 19, gender: "Male" },
-  { name: "Ahmet", surname: "Yılmaz", age: 19, gender: "Male" },
-];
+const styles = makeStyles({
+  layout: {
+    width: "50%",
+    margin: "auto",
+    textAlign: "center",
+    marginTop: "100px",
+  },
+});
 
-function App() {
+function Admin() {
+  const classes = styles();
+
   return (
-    <div className="adminPanel">
-      <table>
-        <tr>
-          <th>Ad</th>
-          <th>Soyad</th>
-          <th>Cinsiyet</th>
-          <th>RCY</th>
-        </tr>
-        {data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.name}</td>
-              <td>{val.age}</td>
-              <td>{val.gender}</td>
-            </tr>
-          );
-        })}
-      </table>
+    <div className="App">
+      <div className={classes.layout}>
+        <UserTbl />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Admin;
