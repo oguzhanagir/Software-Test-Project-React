3dbc919d896978bf2605ea2b4404da6630abf36c
cheguidev <redmagicman67@outlook.com>
2022-12-21 10:26:48 +0300

    Admin panel improvements

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -2,13 +2,15 @@ import React from "react";
 import UserTbl from "../components/UserTbl";
 import { makeStyles } from "@material-ui/core";
 import PersonnelTbl from "../components/PersonnelTbl";
+import { Link } from "react-router-dom";
 
 const styles = makeStyles({
   layout: {
     width: "50%",
     margin: "auto",
     textAlign: "center",
-    marginTop: "100px",
+    marginTop: "50px",
+    marginBottom: "50px",
   },
 });
 
@@ -17,6 +19,26 @@ function Admin() {
 
   return (
     <div className="App">
+      <div className="parent">
+        <h1
+          style={{
+            width: "100%",
+            backgroundColor: "lightBlue",
+            textAlign: "left",
+          }}
+        >
+          Admin Paneli
+        </h1>
+        <Link to={"/"} style={{ textDecoration: "none", color: "#FFF" }}>
+          <button
+            style={{ width: "200px", height: "48px" }}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Çıkış Yap
+          </button>
+        </Link>
+      </div>
       <div className={classes.layout}>
         <h1 style={{ color: "red" }}>Görevliler</h1>
         <PersonnelTbl />
